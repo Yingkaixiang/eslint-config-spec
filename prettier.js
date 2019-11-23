43e5a58608c161b04bdc02be8e46eab7276b57fa
Yingkaixiang <rubykaikai@gmail.com>
2019-11-23 14:01:08 +0800

    feat: Optimize some javascript rules

---
+++ prettier.js
@@ -0,0 +1,49 @@
+module.exports = {
+  // eslint-config-prettier
+  // 用于关闭所有会和 prettier 冲突的规则
+  extends: ['prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        // 每行最多 80 个字符
+        printWidth: 80,
+        // 缩进使用 2 个空格
+        tabWidth: 2,
+        // 缩进使用 tabs 代替空格
+        useTabs: false,
+        // 每条语句末尾添加分号
+        semi: true,
+        // 使用单引号替换双引号
+        singleQuote: true,
+        // 对象的 key 仅在必要时用引号
+        quoteProps: 'as-needed',
+        // jsx 不使用单引号，而使用双引号
+        jsxSingleQuote: false,
+        // 多行结尾添加逗号
+        trailingComma: 'all',
+        // 大括号内的首尾需要空格
+        bracketSpacing: true,
+        // jsx 标签的反尖括号需要换行
+        jsxBracketSameLine: false,
+        // 箭头函数，只有一个参数的时候，也需要括号
+        // 迁移到 typescript 的时候会很便利
+        arrowParens: 'always',
+        // 每个文件格式化的范围是文件的全部内容
+        rangeStart: 0,
+        rangeEnd: Infinity,
+        // 不需要写文件开头的 @prettier
+        requirePragma: false,
+        // 不需要自动在文件开头插入 @prettier
+        insertPragma: false,
+        // 使用默认的折行标准
+        proseWrap: 'preserve',
+        // 根据显示样式决定 html 要不要折行
+        htmlWhitespaceSensitivity: 'css',
+        // 换行符使用 lf
+        endOfLine: 'lf',
+      },
+    ],
+  },
+};
--- test/javascript/no-process-exit/.eslintrc.js
+++ test/javascript/no-process-exit/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    rules: {
-        /**
-         * 禁止使用 process.exit(0)
-         */
-        'no-process-exit': 'off'
-    }
+  rules: {
+    /**
+     * 禁止使用 process.exit(0)
+     */
+    "no-process-exit": "off"
+  }
 };
